4d89545c9b2f2c36b1b7e29d58cf560b03fafc45
David Quenault <david.quenault@uk.tesco.com>
2012-07-25 19:12:07 +0100

    Initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '4e1c6d3f80bc0234d3813330f554e67c796e439d52e8a332d1de4155402802834f314b7406f37d03201161e9d172ee6eb5f06393fdaaccff3ca8549780354166'
